40f104bdf161e7e36d9d4ce569ce9a037b1c24f3
Jguillenencinas <jguillenencinas@gmail.com>
2022-06-21 12:41:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-u7qwar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-u7qwar)
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCfh1HgdKEwqVVymXJc-x-WpZCF1rRP5QA',
+    authDomain: 'miss-pichuchis.firebaseapp.com',
+    projectId: 'miss-pichuchis',
+    storageBucket: 'miss-pichuchis.appspot.com',
+    messagingSenderId: '408359815093',
+    appId: '1:408359815093:web:84aeb4cc0c125cf849fbfc',
+    measurementId: 'G-NQZXEELHD3',
+  },
+};
